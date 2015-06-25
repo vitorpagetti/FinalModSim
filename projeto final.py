796644271dcf0301a241192f86d859227a6dfeec
vitorpagetti <vipagetti@hotmail.com>
2015-06-25 17:00:45 -0300

    ARRUMAR O LINSPACE

--- projeto final.py
+++ projeto final.py
@@ -3,22 +3,22 @@ from scipy.integrate import odeint
 from math import sqrt
 from numpy import linspace
 
-m1 = 4353
-m2 = 3000
-m3 = 1500
-x1 = 500
-y1 = 300
-x2 = 800
-y2 = 2000
-x3 = 1100
-y3 = 700
+m1 = 55000000
+m2 = 850000000
+m3 = 650000000
+x1 = 500000
+y1 = 600000
+x2 = 800000
+y2 = 900000
+x3 = 700000
+y3 = 700000
 G = 67428*(10**-11)
-Vx1 = 600
-Vy1 = 320
-Vx2 = 400
-Vy2 = 450
-Vx3 = 700
-Vy3 = 200
+Vx1 = 3000
+Vy1 = 3200
+Vx2 = 1000
+Vy2 = 4500
+Vx3 = 6000
+Vy3 = 6000
 
 def func(X, t):
     x1 = X[1]
@@ -44,10 +44,10 @@ def func(X, t):
     F23y = k23 * (x23/((sqrt(x23**2+y23**2))))
     dVx1dt = (F12x + F13x)/m1
     dVy1dt = (F12y + F13y)/m1
-    dVx2dt = (F12x + F23x)/m2
-    dVy2dt = (F12y + F23y)/m2
-    dVx3dt = (F13x + F23x)/m3
-    dVy3dt = (F13y + F23y)/m3
+    dVx2dt = (-F12x + F23x)/m2
+    dVy2dt = (-F12y + F23y)/m2
+    dVx3dt = (-F13x - F23x)/m3
+    dVy3dt = (-F13y - F23y)/m3
     dx1dt = X[0]
     dy1dt = X[2]
     dx2dt = X[4]
@@ -58,7 +58,7 @@ def func(X, t):
     
 
 X0 = [Vx1, x1, Vy1, y1, Vx2, x2, Vy2, y2, Vx3, x3, Vy3, y3]
-t = linspace(0,10000,10001)
+t = linspace(0,10000000000000000,1000000001)
 X = odeint(func, X0, t)
 
 plt.plot(X[:,1], X[:,3], "r--")
